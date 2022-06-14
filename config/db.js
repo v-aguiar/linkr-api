25ab59815a4da77e1305d69bb8894830a99ecf8a
v-aguiar <v.aguiar.dev@gmail.com>
2022-06-14 19:35:14 -0300

    fix: Change database config to PROD/DEV environment

--- config/db.js
+++ config/db.js
@@ -1,16 +1,21 @@
-﻿import pg from "pg";
+﻿import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
 
-const db = new pg.Pool({
-  connectionString,
-  ssl: {
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.MODE === "PROD") {
+  configDatabase.ssl = {
     rejectUnauthorized: false,
-  },
-});
+  };
+}
+
+const db = new Pool(configDatabase);
 
 db.on("error", (err, client) => {
   console.error("⚠ Could not connect to database! ", err);
