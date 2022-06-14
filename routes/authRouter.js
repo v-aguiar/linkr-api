f802a7242a000b3c3b1b8bb0de91a95646a0b10e
pablodamascenoo <pablofacex@gmail.com>
2022-06-14 20:46:38 -0300

    feat: added login route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,13 @@
 import Router from "express";
+import { login } from "../controllers/authController.js";
 import { createUser } from "../controllers/userController.js";
 import { validateSchema } from "../middlewares/schemaValidator.js";
+import signInSchema from "../schemas/signInSchema.js";
 import signUpSchema from "../schemas/signUpSchema.js";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", validateSchema(signUpSchema), createUser);
+authRouter.post("/sign-in", validateSchema(signInSchema), login);
 
 export default authRouter;
