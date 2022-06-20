67dcf5080dd489a0a6f7914b297e52313a2b9069
Driven Education <marcio.garrido.jr@gmail.com>
2022-06-20 15:33:26 -0300

    feat: bug fix on posts route

--- schemas/timelineSchemas.js
+++ schemas/timelineSchemas.js
@@ -1,13 +1,13 @@
-import joi from "joi";
+import Joi from "joi";
 
 const regexUrl = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
 
 export const createPostSchema = Joi.object().keys({
-    text: joi.string().allow(null, "").required(),
-    url: joi.string().regex(regexUrl).required(),
-    hashtags: joi.array().items(joi.string()).required()
+    text: Joi.string().allow(null, "").required(),
+    url: Joi.string().regex(regexUrl).required(),
+    hashtags: Joi.array().items(Joi.string()).required()
 });
 
-export const handleLikeSchema = joi.object().keys({
-    liked: joi.boolean().required()
+export const handleLikeSchema = Joi.object().keys({
+    liked: Joi.boolean().required()
   });
