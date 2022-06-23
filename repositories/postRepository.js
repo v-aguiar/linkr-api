c4ec4c593312b734102489036ee12a169887d9c6
pablodamascenoo <pablofacex@gmail.com>
2022-06-23 12:09:21 -0300

    feat: added url-metadata

--- repositories/postRepository.js
+++ repositories/postRepository.js
@@ -1,27 +1,26 @@
 import db from "../config/db.js";
 
+async function createPosts(userId, url, text, metadata) {
+    const { image, title, description } = metadata;
 
-
-async function createPosts(userId, url, text){
     return db.query(
-        `INSERT INTO posts ("userId", url, text) VALUES($1, $2, $3)`,
-        [userId, url, text]
-    )
+        `INSERT INTO posts ("userId", url, text, "imgUrl", title, description) VALUES($1, $2, $3, $4, $5, $6)`,
+        [userId, url, text, image, title, description]
+    );
 }
 
-async function showPosts(){
+async function showPosts() {
     return db.query(
         `SELECT p.id, u.username, p.text, p.url, u."imgUrl" FROM posts p 
         JOIN users u 
         ON u.id = p."userId" 
         ORDER BY p."createdAt" desc LIMIT 20`
-    )
+    );
 }
-
 
 const postsRepository = {
     createPosts,
-    showPosts
-}
+    showPosts,
+};
 
-export default postsRepository
+export default postsRepository;
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,13 +2,14 @@ import Router from "express";
 import { createPost, getPosts } from "../controllers/postController.js";
 import { validateToken } from "../middlewares/authValidator.js";
 import { validateSchema } from "../middlewares/schemaValidator.js";
+import { validateUrl } from "../middlewares/urlValidator.js";
 import postSchema from "../schemas/postsSchema.js";
 
 
 
 const postsRouter = Router();
 
-postsRouter.post("/posts", validateSchema(postSchema), validateToken, createPost)
+postsRouter.post("/posts", validateToken, validateSchema(postSchema), validateUrl ,createPost)
 postsRouter.get("/posts", getPosts)
 
 export default postsRouter
